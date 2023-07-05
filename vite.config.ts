b6f78ead94833c84ae9cee2cc0bdde8facbf914f
Jason Tsorlinis <jason.tsorlinis@jbhifi.com.au>
2023-07-06 09:29:18 +1000

    split konva chunk out

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          konva: ["react-konva", "konva"],
+        },
+      },
+    },
+  },
+});
