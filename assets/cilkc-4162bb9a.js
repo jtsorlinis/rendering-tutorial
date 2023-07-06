48f9aaea2450a9ad6ad7093d8f4fc9438c71035b
Jason Tsorlinis <jason.tsorlinis@jbhifi.com.au>
2023-07-07 09:45:08 +1000

    Updates

---
+++ assets/cilkc-4162bb9a.js
@@ -0,0 +1,1 @@
+import e from"./c-6366ae4f.js";import"./index-583176fc.js";import"./konva-e1600009.js";c.displayName="cilkc";c.aliases=["cilk-c"];function c(a){a.register(e),a.languages.cilkc=a.languages.insertBefore("c","function",{"parallel-keyword":{pattern:/\bcilk_(?:for|reducer|s(?:cope|pawn|ync))\b/,alias:"keyword"}}),a.languages["cilk-c"]=a.languages.cilkc}export{c as default};
