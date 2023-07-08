26d044ba8f683636b94a1d7e0d7ae048f2dec52a
Jason Tsorlinis <jtsorlinis@gmail.com>
2023-07-08 10:31:57 +1000

    cleaned up triangle code

--- src/snippets.ts
+++ src/snippets.ts
@@ -42,42 +42,50 @@ const pointInTriangle3 = `if (ABP >= 0 && BCP >= 0 && CAP >= 0) {
   // Point is inside the triangle ABC
 }`;
 
-const drawTriangle1 = `const P = new Point(0, 0);
+const drawTriangle1 = `const drawTriangle(A: Point, B: Point, C: Point) {
+  // Initialise a point variable
+  const P = new Point(0, 0);
 
-// Loop through all the pixels of the canvas
-for (P.y = 0; P.y < canvas.height; P.y++) {
-  for (P.x = 0; P.x < canvas.width; P.x++) {
-    // Calculate our edge functions
-    const ABP = edgeFunction(A, B, P);
-    const BCP = edgeFunction(B, C, P);
-    const CAP = edgeFunction(C, A, P);
+  // Loop through all the pixels of the canvas
+  for (P.y = 0; P.y < canvas.height; P.y++) {
+    for (P.x = 0; P.x < canvas.width; P.x++) {
+      // Calculate our edge functions
+      const ABP = edgeFunction(A, B, P);
+      const BCP = edgeFunction(B, C, P);
+      const CAP = edgeFunction(C, A, P);
 
-    // If all the edge functions are positive, the point is inside the triangle
-    if (ABP >= 0 && BCP >= 0 && CAP >= 0) {
-      // Draw the pixel
-      setPixel(P.x, P.y, "red");
+      // If all the edge functions are positive, the point is inside the triangle
+      if (ABP >= 0 && BCP >= 0 && CAP >= 0) {
+        // Draw the pixel
+        setPixel(P.x, P.y, "red");
+      }
     }
   }
 }`;
 
-const boundingBox = `// Get the bounding box of the triangle
-const minX = Math.min(A.x, B.x, C.x);
-const minY = Math.min(A.y, B.y, C.y);
-const maxX = Math.max(A.x, B.x, C.x);
-const maxY = Math.max(A.y, B.y, C.y);
+const boundingBox = `const drawTriangle(A: Point, B: Point, C: Point) {
+  // Initialise a point variable
+  const P = new Point(0, 0);
+  
+  // Get the bounding box of the triangle
+  const minX = Math.min(A.x, B.x, C.x);
+  const minY = Math.min(A.y, B.y, C.y);
+  const maxX = Math.max(A.x, B.x, C.x);
+  const maxY = Math.max(A.y, B.y, C.y);
 
-// Loop through all the pixels of the bounding box
-for (P.y = minY; P.y < maxY; P.y++) {
-  for (P.x = minX; P.x < maxX; P.x++) {
-    // Calculate our edge functions
-    const ABP = edgeFunction(A, B, P);
-    const BCP = edgeFunction(B, C, P);
-    const CAP = edgeFunction(C, A, P);
+  // Loop through all the pixels of the bounding box
+  for (P.y = minY; P.y < maxY; P.y++) {
+    for (P.x = minX; P.x < maxX; P.x++) {
+      // Calculate our edge functions
+      const ABP = edgeFunction(A, B, P);
+      const BCP = edgeFunction(B, C, P);
+      const CAP = edgeFunction(C, A, P);
 
-    // If all the edge functions are positive, the point is inside the triangle
-    if (ABP >= 0 && BCP >= 0 && CAP >= 0) {
-      // Draw the pixel
-      setPixel(P.x, P.y, "red");
+      // If all the edge functions are positive, the point is inside the triangle
+      if (ABP >= 0 && BCP >= 0 && CAP >= 0) {
+        // Draw the pixel
+        setPixel(P.x, P.y, "red");
+      }
     }
   }
 }`;
@@ -117,56 +125,53 @@ const b = colourA.b * weightA + colourB.b * weightB + colourC.b * weightC;
 const colourP = new Colour(r, g, b);
 `;
 
-const final = `// Create our points
-const A = new Point(50, 450);
-const B = new Point(250, 50);
-const C = new Point(450, 450);
-
-// Create our colours
+const final = `// Create our colours
 const colourA = new Colour(255, 0, 0); // Red
 const colourB = new Colour(0, 255, 0); // Green
 const colourC = new Colour(0, 0, 255); // Blue
 
-// Get the bounding box of the triangle
-const minX = Math.min(A.x, B.x, C.x);
-const minY = Math.min(A.y, B.y, C.y);
-const maxX = Math.max(A.x, B.x, C.x);
-const maxY = Math.max(A.y, B.y, C.y);
+const drawTriangle(A: Point, B: Point, C: Point) { 
+  // Calculate the edge function for the whole triangle (ABC)
+  const ABC = edgeFunction(A, B, C);
 
-// Calculate the edge function for the whole triangle (ABC)
-const ABC = edgeFunction(A, B, C);
+  // Our nifty trick: Don't bother drawing the triangle if it's back facing
+  if (ABC < 0) {
+    return;
+  }
 
-// Our nifty trick: Don't bother drawing the triangle if it's back facing
-if (ABC < 0) {
-  return;
-}
+  // Initialise our point
+  const P = new Point(0, 0);
 
-// Initialise our point
-const P = new Point(0, 0);
+  // Get the bounding box of the triangle
+  const minX = Math.min(A.x, B.x, C.x);
+  const minY = Math.min(A.y, B.y, C.y);
+  const maxX = Math.max(A.x, B.x, C.x);
+  const maxY = Math.max(A.y, B.y, C.y);
 
-// Loop through all the pixels of the bounding box
-for (P.y = minY; P.y < maxY; P.y++) {
-  for (P.x = minX; P.x < maxX; P.x++) {
-    // Calculate our edge functions
-    const ABP = edgeFunction(A, B, P);
-    const BCP = edgeFunction(B, C, P);
-    const CAP = edgeFunction(C, A, P);
+  // Loop through all the pixels of the bounding box
+  for (P.y = minY; P.y < maxY; P.y++) {
+    for (P.x = minX; P.x < maxX; P.x++) {
+      // Calculate our edge functions
+      const ABP = edgeFunction(A, B, P);
+      const BCP = edgeFunction(B, C, P);
+      const CAP = edgeFunction(C, A, P);
 
-    // Normalise the edge functions by dividing by the total area to get the barycentric coordinates
-    const weightA = BCP / ABC;
-    const weightB = CAP / ABC;
-    const weightC = ABP / ABC;
+      // Normalise the edge functions by dividing by the total area to get the barycentric coordinates
+      const weightA = BCP / ABC;
+      const weightB = CAP / ABC;
+      const weightC = ABP / ABC;
 
-    // If all the edge functions are positive, the point is inside the triangle
-    if (ABP >= 0 && BCP >= 0 && CAP >= 0) {
-      // Interpolate the colours at point P
-      const r = colourA.r * weightA + colourB.r * weightB + colourC.r * weightC;
-      const g = colourA.g * weightA + colourB.g * weightB + colourC.g * weightC;
-      const b = colourA.b * weightA + colourB.b * weightB + colourC.b * weightC;
-      const colourP = new Colour(r, g, b);
+      // If all the edge functions are positive, the point is inside the triangle
+      if (ABP >= 0 && BCP >= 0 && CAP >= 0) {
+        // Interpolate the colours at point P
+        const r = colourA.r * weightA + colourB.r * weightB + colourC.r * weightC;
+        const g = colourA.g * weightA + colourB.g * weightB + colourC.g * weightC;
+        const b = colourA.b * weightA + colourB.b * weightB + colourC.b * weightC;
+        const colourP = new Colour(r, g, b);
 
-      // Draw the pixel
-      setPixel(P.x, P.y, "red");
+        // Draw the pixel
+        setPixel(P.x, P.y, "red");
+      }
     }
   }
 }`;
