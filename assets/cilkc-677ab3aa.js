eac086e6090a66499f23a66f706e95412e3e2b07
Jason <jtsorlinis@gmail.com>
2026-03-30 15:13:12 +1100

    Updates

---
+++ assets/cilkc-677ab3aa.js
@@ -0,0 +1,1 @@
+import e from"./c-fc86a91f.js";import"./index-937ca1db.js";import"./konva-6694c3a9.js";c.displayName="cilkc";c.aliases=["cilk-c"];function c(a){a.register(e),a.languages.cilkc=a.languages.insertBefore("c","function",{"parallel-keyword":{pattern:/\bcilk_(?:for|reducer|s(?:cope|pawn|ync))\b/,alias:"keyword"}}),a.languages["cilk-c"]=a.languages.cilkc}export{c as default};
